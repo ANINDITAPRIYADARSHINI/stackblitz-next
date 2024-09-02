4b081ffd92650c64267d5e5efdeceb2240a30eb2
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2024-09-02 20:54:35 +0530

    fixed image bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+'use client'
+
+import React from 'react'
 import AcmeLogo from '@/app/ui/acme-logo'
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
+/*import styles from '@/app/ui/home.module.css';*/
 import {lusitana} from '@/app/ui/fonts';
 import Image from 'next/image';
 
@@ -9,6 +12,12 @@ import Image from 'next/image';
 
 
 export default function Page() {
+  const [isOptimized, setIsOptimized] = React.useState(true);
+
+
+
+
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -38,7 +47,12 @@ export default function Page() {
             width={1000}
             height={760}
             alt="Screenshots of the dashboard project showing desktop version"
+            unoptimized={!isOptimized}
             className="hidden md:block"
+            onError={() => {
+              setIsOptimized(false);
+            }}
+            priority={false}
           />
           
           <Image
@@ -46,7 +60,11 @@ export default function Page() {
             width={560}
             height={620}
             alt="Screenshots of the dashboard project showing mobile version"
-            className="block md:hidden"
+            unoptimized={!isOptimized}
+            className="md:hidden"
+            onError={() => {
+              setIsOptimized(false);
+            }}
           />
         </div>
       </div>
